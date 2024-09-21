e5917957cac82d550e4951ca12db1aead3e85d6f
Andre78-lab <asolo3@yandex.ru>
2024-09-21 07:59:52 +0300

    Add files via upload

--- bab2.py
+++ bab2.py
@@ -2,7 +2,12 @@ number_dz = 12
 number_dz_h = 1.5
 course_name = "Python"
 time_1_dz = 1.5 / 12
-print("Количество выполненных ДЗ - ", number_dz)
-print("Количество затраченных часов - ", number_dz_h)
-print("Название курса - ", course_name)
-print("Среднее время потрачено на одно задание - ", time_1_dz)
+hours_n = int(time_1_dz)
+minutes_dr = (time_1_dz-hours_n)*60
+minutes_n = int(minutes_dr)
+seconds_n =int((minutes_dr - minutes_n)*60)
+
+print("Количество выполненных ДЗ -", number_dz)
+print("Количество затраченных часов -", number_dz_h)
+print("Название курса -", course_name)
+print("Среднее время потрачено на одно задание - ", time_1_dz, " часа или ", hours_n, "ч", minutes_n, "мин", seconds_n, "сек" )
