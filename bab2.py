d87996f456526362c2b660cbcb772c65f683d4c9
Andre78-lab <asolo3@yandex.ru>
2024-09-21 08:14:14 +0300

    Add files via upload

--- bab2.py
+++ bab2.py
@@ -11,3 +11,5 @@ print("Количество выполненных ДЗ -", number_dz)
 print("Количество затраченных часов -", number_dz_h)
 print("Название курса -", course_name)
 print("Среднее время потрачено на одно задание - ", time_1_dz, " часа или ", hours_n, "ч", minutes_n, "мин", seconds_n, "сек" )
+
+print("Курс:",course_name, "всего задач:", number_dz, "затрачено часов:", number_dz_h, "среднее время выполнения", time_1_dz, " часа или ", hours_n, "ч", minutes_n, "мин", seconds_n, "сек")
