7ba4eda97654cc4010876d5a4ad7764e5fba6a41
Andre78-lab <asolo3@yandex.ru>
2024-09-21 07:35:27 +0300

    Add files via upload

---
+++ bab2.py
@@ -0,0 +1,8 @@
+number_dz = 12
+number_dz_h = 1.5
+course_name = "Python"
+time_1_dz = 1.5 / 12
+print("Количество выполненных ДЗ - ", number_dz)
+print("Количество затраченных часов - ", number_dz_h)
+print("Название курса - ", course_name)
+print("Среднее время потрачено на одно задание - ", time_1_dz)
