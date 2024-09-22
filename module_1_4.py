6a20143c69d65596b1016c6eed8a5f63b7bd8706
Andre78-lab <asolo3@yandex.ru>
2024-09-22 09:21:48 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('введиет произвольный текст: ')
+print('количество символов введённого текста -', len(my_string))
+print('cтрока в верхнем регистре -', my_string.upper())
+print('строка в нижнем регистре -', my_string.lower())
+print('без пробелов -', my_string.replace(' ',''))
+print('первый символ строки - ', my_string[0])
+print('последний символ строки - ', my_string[-1])
