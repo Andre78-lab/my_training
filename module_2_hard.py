2c76b74bcfbfe37305de9967a1cbd2d5b9409425
Andre78-lab <asolo3@yandex.ru>
2024-10-03 10:17:34 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,11 @@
+import random
+
+stone_1 = random.randint(3,20)
+print(f'Первый поле, камень {stone_1}')
+str_i = "Пары чисел для второго поля: "
+for i in range(1, int(stone_1/2)+1):
+    for j in range(i+1, stone_1):
+        if stone_1 % (i+j) == 0:
+            str_i += f' {i} и {j}, '
+
+print(str_i[:-2])
