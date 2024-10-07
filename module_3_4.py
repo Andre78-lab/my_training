f990fa7f7e4e266c08591b1dc761bcdc3934319b
Andre78-lab <asolo3@yandex.ru>
2024-10-07 10:43:52 +0300

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,14 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for i_word in other_words:
+        if root_word.lower() in i_word.lower() or i_word.lower() in root_word.lower():
+            same_words.append(i_word)
+    return same_words
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+result3 = single_root_words('Лен', 'поленивавшийся', 'Лень', 'ПОЛЕНО', 'СУЧЕК', 'лиса', 'len', 'Полдень', 'разленивающийся')
+print(result1)
+print(result2)
+print(result3)
