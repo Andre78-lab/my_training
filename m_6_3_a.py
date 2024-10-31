8b95e6ae8feac8a5a3d5a8b7d5d2d5c2b627dafb
Andre78-lab <asolo3@yandex.ru>
2024-10-31 11:01:47 +0300

    Add files via upload

---
+++ m_6_3_a.py
@@ -0,0 +1,63 @@
+class Horse:
+    """    Класс описание лошади    """
+
+    def __init__(self, *args):
+        self.x_distance = 0
+        self.sound = 'Frrr'
+        super().__init__(*args)
+
+    def run(self, dx):  # Изменение дистанции
+        self.x_distance += dx
+
+
+class Eagle:
+    """    Класс описание орла    """
+
+    def __init__(self, *args):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
+
+    def fly(self, dy):  # Изменение высоты
+        self.y_distance += dy
+
+
+# class Pegasus(Horse, Eagle):
+#     """    Класс пегаса наследует от Horse и Eagle (в таком порядке)   """
+#
+#     def __init__(self, *args):
+#         super().__init__(*args)
+#
+#     def move(self, dx, dy):
+#         self.run(dx)
+#         self.fly(dy)
+#
+#     def get_pos(self):
+#         return self.x_distance, self.y_distance
+#
+#     def voice(self):
+#         print(self.sound)
+
+
+#print(Horse.mro())
+#print(Eagle.mro())
+#print(Pegasus.mro())
+#
+# p1 = Pegasus()
+#
+# print(p1.get_pos())
+# p1.move(10, 15)
+# print(p1.get_pos())
+# p1.move(-5, 20)
+# print(p1.get_pos())
+#
+# p1.voice()
+
+p2 = Horse()
+p2.run(20)
+print(p2.x_distance)
+print(p2)
+
+p3 = Eagle()
+p3.fly(30)
+print(p3.y_distance)
+print(p3)
