29352cc195170b1f08c02940046e106e763069b8
Andre78-lab <asolo3@yandex.ru>
2025-01-16 13:54:01 +0300

    Add files via upload

--- module_14_2.py
+++ module_14_2.py
@@ -23,15 +23,15 @@ balance INTEGER NOT NULL
 cursor.execute(" CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
 
 # Заполнение таблицы
-#for i in range(10):
+# for i in range(10):
 #   cursor.execute(" INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
 #                  (f"Новый пользователь {i+1}", f"example{i+1}@gmail.com", f"{(i+1)*10}", str(1000)))
 
 # Обновление balance у каждой 2ой записи начиная с 1ой на 500
-#cursor.execute(" UPDATE Users SET balance = ? WHERE id % 2 = 1", (500,))
+# cursor.execute(" UPDATE Users SET balance = ? WHERE id % 2 = 1", (500,))
 
 # Удаление каждой 3ей записи в таблице начиная с 1ой
-#cursor.execute(" DELETE FROM Users WHERE id % 3 = 1")
+# cursor.execute(" DELETE FROM Users WHERE id % 3 = 1")
 
 # Выборка, где возраст не равен 60
 cursor.execute(" SELECT username, email, age, balance FROM Users WHERE age <> ?", (60,))
@@ -47,23 +47,17 @@ cursor.execute(" SELECT COUNT(*) FROM Users")
 count_users = cursor.fetchone()[0]
 print(f'Общее количество записей - {count_users}')
 
-#Посчитать сумму всех балансов
+# Посчитать сумму всех балансов
 cursor.execute(" SELECT SUM(balance) FROM Users")
 sum_balans_all = cursor.fetchone()[0]
 print(f'Сумма всех балансов - {sum_balans_all}')
 
-#Вывести в консоль средний баланс всех пользователей
-print(f'Средний баланс (расчетный): {sum_balans_all/count_users}')
+# Вывести в консоль средний баланс всех пользователей
+print(f'Средний баланс (расчетный): {sum_balans_all / count_users}')
 
 cursor.execute(" SELECT AVG(balance) FROM Users")
 balance_avg = cursor.fetchone()[0]
 print(f'Средний баланс (через AVG): {balance_avg}')
-
-
-
-
-
-
 
 # !сохранение изменения в базе данных
 connection.commit()
